6e455874eee0a54f366b64ea94e1398702009d56
cianjohn <83083026+cianjohn@users.noreply.github.com>
2021-09-20 12:07:58 +0100

    flask route/render template/and url_for are working as intended

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import (Flask, flash, render_template, redirect, url_for)
+
+random_garbage = Flask(__name__)
+
+@random_garbage.route("/")
+def functionThing():
+    return render_template('index.html')
